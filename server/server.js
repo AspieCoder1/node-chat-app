8d7e9c8cc6d0944f2165cedf4f435cfdf050e266
sai109 <lukebraithwaite7@gmail.com>
2018-02-08 18:23:01 +0000

    Events added for real app

--- server/server.js
+++ server/server.js
@@ -1,11 +1,34 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
-  console.log(`Server up on port ${port}`)
+
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	socket.emit('newMessage', {
+		from: 'example@gmail.com',
+		text: 'Hello',
+		createdAt: '27/02/17 at 15:00'
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log(JSON.stringify(message, undefined, 2));
+	});
+
+	socket.on('disconnect', () => {
+		console.log('Client disconnected');
+	});  
+});
+
+server.listen(port, () => {
+	console.log(`Server up on port ${port}`);
 });
