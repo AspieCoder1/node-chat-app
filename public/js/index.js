be19610e3801edcae5a63d10d3044629c8627046
sai109 <lukebraithwaite7@gmail.com>
2018-02-10 16:46:55 +0000

    CSS added for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -26,11 +26,12 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (e) {
 	e.preventDefault();
+	var messageTextbox = jQuery('[name=message]');
 	socket.emit('createMessage', {
 		from: 'User',
-		text: jQuery('[name=message]').val()
+		text: messageTextbox.val()
 	}, function () {
-
+		messageTextbox.val('');
 	});
 });
 
@@ -40,13 +41,17 @@ locationButton.on('click', function () {
 		return alert('Geolocation not supported by your browser');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Send Location...');
+
 	navigator.geolocation.getCurrentPosition(function(position) {
 		console.log(position);
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
+		locationButton.removeAttr('disabled').text('Send Location');
 	}, function () {
 		alert('Unable to fetch location');
+		locationButton.removeAttr('disabled').text('Send Location');
 	});
 });
